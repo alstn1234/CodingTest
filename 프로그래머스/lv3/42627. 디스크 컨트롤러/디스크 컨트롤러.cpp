74bcbc75f10c2ddb98e233dafd861ee2c13e26e6
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-01-13 01:53:06 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -14,27 +14,24 @@ int solution(vector<vector<int>> jobs) {
     time += (jobs[0][1] + jobs[0][0]);
     answer += time - jobs[0][0];
     jobs.erase(jobs.begin());
-    
     while(!jobs.empty()){
         int n = 0;
         for(int i = 0; i < jobs.size(); i++){
-            if(jobs[i][0] <= time){
-                n = i;
-                continue;
+            if(jobs[i][0] > time){
+                if(time < jobs[0][0])
+                    time = jobs[0][0];
+                break;
             }
-            if(time < jobs[0][0])
-                time = jobs[0][0];
-            break;
+            n = i;
         }
-        
         int next = 0;
         for(int i = 1; i <= n; i++){
             if(jobs[next][1] >= jobs[i][1])
                 next = i;
         }
-            
+        
         time += jobs[next][1];
-        answer += (time - jobs[next][0]);
+        answer += time - jobs[next][0];
         jobs.erase(jobs.begin()+next);
     }
     return answer/size;
