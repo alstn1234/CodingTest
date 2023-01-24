c38987edd003ac75db7051b94ad41f8ba75f04bb
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-01-24 17:24:10 +0900

    [unrated] Title: 시소 짝꿍, Time: 0.01 ms, Memory: 4.03 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/152996. 시소 짝꿍/시소 짝꿍.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(vector<int> weights) {
+    long long answer = 0;
+    long long num = 0;
+    sort(weights.rbegin(), weights.rend());
+    
+    for(int i = 0; i < weights.size() - 1; i++){
+        if(num > 0 && (weights[i] == weights[i-1])){
+            num--;
+            answer += num;
+            continue;
+        }
+        num = 0;
+        for(int j = i + 1; j < weights.size(); j++){
+            double w = (double)weights[i] / (double)weights[j];
+            if(w == 1){
+                num++;
+                continue;
+            }
+            if(w == 3.0/2.0){
+                num++;
+                continue;
+            }
+            if(w == 4.0/3.0){
+                num++;
+                continue;
+            }
+            if(w== 2){
+                num++;
+                continue;
+            }
+        }
+        answer += num;
+    }
+    return answer;
+}
