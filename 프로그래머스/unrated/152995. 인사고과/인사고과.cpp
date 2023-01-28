456cc59d88296b256d048d034ec0dd085f573842
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-01-29 01:31:01 +0900

    [unrated] Title: 인사고과, Time: 2726.05 ms, Memory: 21.4 MB -BaekjoonHub

--- 프로그래머스/unrated/152995. 인사고과/인사고과.cpp
+++ 프로그래머스/unrated/152995. 인사고과/인사고과.cpp
@@ -6,10 +6,8 @@ using namespace std;
 
 int solution(vector<vector<int>> scores) {
     int answer = 0;
-    vector<int> score_total;
     vector<int> inho = scores[0];
     int inho_total = inho[0] + inho[1];
-    score_total.push_back(inho_total);
     for(auto s : scores){
         if(inho[0] < s[0] && inho[1] < s[1])
             return -1;
@@ -18,12 +16,9 @@ int solution(vector<vector<int>> scores) {
                 if(s[0] < scores[j][0] && s[1] < scores[j][1])
                     break;
                 if(j == scores.size() - 1)
-                    score_total.push_back(s[0] + s[1]);
+                    answer++;
             }
         }
     }
-    sort(score_total.rbegin(), score_total.rend());
-    for(int i = 0; i < score_total.size(); i++)
-        if(score_total[i] == inho_total)
-            return i + 1;
+    return ++answer;
 }
