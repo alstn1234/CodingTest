e625666190437923a06193647bbae413eaadbefc
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-01-28 19:46:57 +0900

    [unrated] Title: 무인도 여행, Time: 0.09 ms, Memory: 4.03 MB -BaekjoonHub

--- 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.cpp
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.cpp
@@ -10,24 +10,20 @@ int dfs(vector<string> maps, int pos_x, int pos_y, vector<vector<bool>>& visited
     visited[pos_x][pos_y] = true;
     //좌
     if (pos_y >= 1 && maps[pos_x][pos_y - 1] != 'X'
-        && !visited[pos_x][pos_y - 1]) {
+        && !visited[pos_x][pos_y - 1]) 
         count += dfs(maps, pos_x, pos_y - 1, visited, maps[pos_x][pos_y - 1] - '0');
-    }
     //우
     if (pos_y <= maps[0].size() - 2 && maps[pos_x][pos_y + 1] != 'X'
-        && !visited[pos_x][pos_y + 1]) {
+        && !visited[pos_x][pos_y + 1]) 
         count += dfs(maps, pos_x, pos_y + 1, visited, maps[pos_x][pos_y + 1] - '0');
-    }
     //상
     if (pos_x >= 1 && maps[pos_x - 1][pos_y] != 'X'
-        && !visited[pos_x - 1][pos_y]) {
+        && !visited[pos_x - 1][pos_y]) 
         count += dfs(maps, pos_x - 1, pos_y, visited, maps[pos_x - 1][pos_y] - '0');
-    }
     //하
     if (pos_x <= maps.size() - 2 && maps[pos_x + 1][pos_y] != 'X'
-        && !visited[pos_x + 1][pos_y]) {
+        && !visited[pos_x + 1][pos_y]) 
         count += dfs(maps, pos_x + 1, pos_y, visited, maps[pos_x + 1][pos_y] - '0');
-    }
     return count;
 }
 
