881b0155cc91c2e7ed95b27c2bf9179da464fb44
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-02-04 15:33:29 +0900

    [unrated] Title: 호텔 대실, Time: 0.29 ms, Memory: 4.11 MB -BaekjoonHub

--- 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.cpp
+++ 프로그래머스/unrated/155651. 호텔 대실/호텔 대실.cpp
@@ -8,33 +8,25 @@ using namespace std;
 int solution(vector<vector<string>> book_time) {
     int n = 0;
     int room_cnt = 0;
-    vector<string> book;
+    vector<pair<int, char>> book;
     for(int i = 0; i < book_time.size(); i++){
-        book.push_back(book_time[i][0] + 'B');  // B : CheckIn
-        int h = stoi(book_time[i][1].substr(0,2));
-        int m = stoi(book_time[i][1].substr(3,2));
-        m += 10;
-        if(m >= 60){
-            m -= 60;
-            h += 1;
-        }
-        string mm = to_string(m);
-        string hh = to_string(h);
-        if(h < 10)
-            hh = '0' + to_string(h);
-        if(m < 10)
-            mm = '0' + to_string(m);
-        book.push_back(hh + ':' + mm + 'A');  // A : CheckOut
+        int h = stoi(book_time[i][0].substr(0,2));
+        int m = stoi(book_time[i][0].substr(3,2));
+        book.push_back(pair<int, char>(h*60+m, 'B'));  // B : CheckIn
+        
+        h = stoi(book_time[i][1].substr(0,2));
+        m = stoi(book_time[i][1].substr(3,2));
+        book.push_back(pair<int, char>(h*60+m+10, 'A'));  // A : CheckOut
     }
-    priority_queue<string, vector<string>, greater<string>> pq(book.begin(), book.end());
+    priority_queue<pair<int, char>, vector<pair<int, char>>, greater<pair<int, char>>> pq(book.begin(), book.end());
     
     
     while(!pq.empty()){
-        string time = pq.top();
+        auto time = pq.top();
         pq.pop();
-        if(time[5] == 'B')
+        if(time.second == 'B')
             room_cnt = max(room_cnt, ++n);
-        else if(time[5] == 'A')
+        else if(time.second == 'A')
             n--;
     }
     return room_cnt;
