5d9ef1384a5d3a8366a291cec610cad4ab670d6c
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-02-05 01:07:55 +0900

    [unrated] Title: 점 찍기, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/140107. 점 찍기/점 찍기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(int k, int d) {
+    long long answer = 0;
+    long long dd = (long long)d * d;
+    for(int i = 0; i <= d; i += k){
+        long long y = sqrt(dd - (long long)i*i);
+        answer += y/(long long)k + 1;
+    }
+    return answer;
+}
