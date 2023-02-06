5b664596a142f2a8a88a254a2b2b49ef2379d7c3
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-02-07 00:35:47 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -21,6 +21,6 @@ string solution(string s) {
     string answer = "";
     vector<int> v = split(s, ' ');
     sort(v.begin(), v.end());
-    answer += to_string(v[0]) + " " + to_string(v[v.size()-1]);
+    answer += to_string(v.front()) + " " + to_string(v.back());
     return answer;
 }
