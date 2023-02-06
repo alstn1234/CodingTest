9055364f34457b8eb299ea573347dd9077a96189
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-02-07 00:35:02 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> split(string input, char del){
+    vector<int> answer;
+    stringstream ss(input);
+    string temp;
+    
+    while(getline(ss, temp, del)){
+        answer.push_back(stoi(temp));
+    }
+    return answer;
+}
+
+string solution(string s) {
+    string answer = "";
+    vector<int> v = split(s, ' ');
+    sort(v.begin(), v.end());
+    answer += to_string(v[0]) + " " + to_string(v[v.size()-1]);
+    return answer;
+}
