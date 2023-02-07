837e63f61f1c9544591ba6ad3cd66b036cf91d0b
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-02-08 01:28:31 +0900

    [level 3] Title: 풍선 터트리기, Time: 3.54 ms, Memory: 38.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+int solution(vector<int> a) {
+    int answer = a.size();
+    int min_a;
+    int n = *min_element(a.begin(), a.end());
+    
+    min_a = a[0];
+    if(min_a != n){
+        for(int i = 1; a[i]!=n; i++){
+            if(min_a < a[i]){
+                answer--;
+            }
+            else
+            min_a = a[i];
+        }
+    }
+    
+    min_a = a.back();
+    if(min_a != n){
+        for(int i = a.size()-2; a[i]!=n; i--){
+            if(min_a < a[i]){
+                answer--;
+            }
+            else
+                min_a = a[i];
+        }
+    }
+    return answer;
+}
