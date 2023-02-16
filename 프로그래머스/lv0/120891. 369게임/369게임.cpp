28639dd292191941609822a7074a75f7dbc9a96c
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-02-16 18:19:01 +0900

    [level 0] Title: 369게임, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int order) {
+    int answer = 0;
+    string s = to_string(order);
+    for(int i = 0; i < s.length(); i++)
+        if(s[i] == '3' || s[i] == '6' || s[i] == '9')
+            answer++;
+    return answer;
+}
