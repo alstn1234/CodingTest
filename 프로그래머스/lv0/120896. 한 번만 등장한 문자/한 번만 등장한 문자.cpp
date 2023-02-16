6c50a73fb57d38a9405ff0b96a192b93a7eec2d7
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-02-16 16:24:27 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    map<char, int> m;
+    for(const char ss : s){
+        m[ss]++;
+    }
+    for(auto it = m.begin(); it != m.end(); it++){
+        if(it->second == 1)
+            answer += it->first;
+    }
+    sort(answer.begin(), answer.end());
+    return answer;
+}
