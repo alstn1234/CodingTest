6edd6ce840d3fbd3c152523ed0433e5a62aba36f
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-02-24 17:39:02 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.cpp
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.cpp
@@ -1,7 +1,6 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 int c;
@@ -15,15 +14,10 @@ int solution(vector<vector<int>> data, int col, int row_begin, int row_end) {
     int answer = 0;
     c = col - 1;
     sort(data.begin(), data.end(), compare);
-    for(auto d : data){
-       // cout << d[0] << d[1] << d[2] << endl;
-    }
     for(int i = row_begin-1; i <= row_end-1; i++){
         int sum = 0;
-        for(int j = 0; j < data[i].size(); j++){
+        for(int j = 0; j < data[i].size(); j++)
             sum += data[i][j]%(i+1);
-            //cout << data[i][j] << " " << i+1 << endl;
-        }
         answer= answer^sum;
     }
     return answer;
