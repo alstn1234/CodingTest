84ba564634435eb477ca71500dfc6a9ebdf55c59
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-02-26 19:10:00 +0900

    [unrated] Title: 우박수열 정적분, Time: 5.10 ms, Memory: 4.51 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134239. 우박수열 정적분/우박수열 정적분.cpp
@@ -0,0 +1,40 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<double> solution(int k, vector<vector<int>> ranges) {
+    vector<double> answer;
+    vector<double> v;
+    while(k != 1){
+        int b_k = k;
+        double area = 0.0;
+        if(k%2 == 0){
+            k /= 2;
+            area += k + (double)(b_k-k)/2;
+        }
+        else{
+            k = k*3+1;
+            area += b_k + (double)(k-b_k)/2;
+        }
+        v.push_back(area);
+    }
+    for(auto vv : v)
+        cout << vv << endl;
+    for(auto range : ranges){
+        int n = range[1]+v.size()-1;
+        if(n+1 < range[0]){
+            answer.push_back(-1);
+            continue;
+        }
+        double sum = 0.0;
+        cout << n << endl;
+        for(int i = range[0]; i <= n; i++){
+            sum += v[i];
+        }
+        answer.push_back(sum);
+    }
+    
+    return answer;
+}
