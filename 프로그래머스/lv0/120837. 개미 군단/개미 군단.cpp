e3859e7de3bb684cba8aaa2241f7498afdec427c
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-03-07 22:12:55 +0900

    [level 0] Title: 개미 군단, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int hp) {
+    int answer = 0;
+    while(hp >= 5){
+        hp -= 5;
+        answer++;
+    }
+    while(hp >= 3){
+        hp -= 3;
+        answer++;
+    }
+    while(hp >= 1){
+        hp -= 1;
+        answer++;
+    }
+    return answer;
+}
