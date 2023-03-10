7807f69343024295510410d62315f7f9a2b4a1ba
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2023-03-10 14:31:30 +0900

    [level 2] Title: [3차] n진수 게임, Time: 0.12 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.cpp
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.cpp
@@ -7,23 +7,20 @@ using namespace std;
 string solve(int n, int size){
     string s = "0";
     for(int i = 1; i <= 1000000; i++){
-        while(size < s.length()){
+        while(size < s.length())
             s.erase(s.length()-1);
-        }
         if(size == s.length())
             break;
         int p = i;
         string ss = "";
+        
         while(p != 0){
             int m = p%n;
-            if(m >= 10){
+            if(m >= 10)
                 ss += 'A'+(m-10);
-                p /= n;
-            }
-            else{
-            ss += m + '0';
+            else
+                ss += m + '0';
             p /= n;
-            }
         }
         reverse(ss.begin(), ss.end());
         s += ss;
@@ -34,6 +31,7 @@ string solve(int n, int size){
 string solution(int n, int t, int m, int p) {
     string answer = "";
     string s = solve(n, t*m);
+    
     for(int i = p-1; i < s.length(); i+=m)
         answer += s[i];
     
