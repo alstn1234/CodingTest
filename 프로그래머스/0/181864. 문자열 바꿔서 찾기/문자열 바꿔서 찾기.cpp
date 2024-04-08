fda3ec840d70c88175de721eec8be01b5f9c2764
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2024-04-08 22:52:08 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.02 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string myString, string pat) {
+    int answer = 0;
+    for(auto& p : pat){
+        if(p == 'A') p = 'B';
+        else p = 'A';
+    }
+    
+    if(myString.find(pat) != string::npos)
+        answer = 1;
+    return answer;
+}
