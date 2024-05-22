acec6e99dc76457662fdcce5c221706f2b36ba1a
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2024-05-22 20:42:40 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 76.44 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.cpp
@@ -6,28 +6,27 @@ using namespace std;
 
 vector<int> solution(vector<string> enroll, vector<string> referral, vector<string> seller, vector<int> amount) {
     vector<int> answer;
-    map<string, int> defaultMoney;
-    map<string, int> addMoney;
+    map<string, int> money;
     map<string, string> parentMap;
+    
     for(int i = 0; i < enroll.size(); i++)
         parentMap[enroll[i]] = referral[i];
     
     for(int i = 0; i < seller.size(); i++){
-        defaultMoney[seller[i]] += amount[i]*90;
-        
-        string parent = parentMap[seller[i]];
+        money[seller[i]] += amount[i]*90;
         int price = amount[i]*10;
+        string parent = parentMap[seller[i]];
         while(parent != "-" && price != 0){
-            addMoney[parent] += price;
+            money[parent] += price;
             price /= 10;
-            defaultMoney[parent] -= price;
+            money[parent] -= price;
             parent = parentMap[parent];
             
         }
     }
     
     for(auto e : enroll){
-        answer.push_back(defaultMoney[e] + addMoney[e]);
+        answer.push_back(money[e]);   
     }
     
     return answer;
