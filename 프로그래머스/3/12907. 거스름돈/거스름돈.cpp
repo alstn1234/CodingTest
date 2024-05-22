7d7c74c720bdcd210e383514e8455a1f99f7577d
alstn1234 <102529677+alstn1234@users.noreply.github.com>
2024-05-22 21:29:33 +0900

    [level 3] Title: 거스름돈, Time: 2.04 ms, Memory: 3.82 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n, vector<int> money) {
+    vector<long long> dp(n+1, 0);
+    dp[0] = 1;
+    for(int i = 0; i < money.size(); i++){
+        for(int j = money[i]; j <= n; j++){
+            dp[j] += dp[j - money[i]];
+        }
+    }
+    return dp[n]%1000000007;
+}
